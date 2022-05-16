933a045f216dcd3574b32aa11ad482db7db0adad
Andrzej Więckowski <andywiecko@gmail.com>
2022-05-16 17:26:00 +0200

    feat: additional mathematics extension

--- Runtime/Core/Extensions/MathematicsExtensions.cs
+++ Runtime/Core/Extensions/MathematicsExtensions.cs
@@ -7,6 +7,7 @@ namespace andywiecko.PBD2D.Core
     {
         public static float3 ToFloat3(this float2 value, float z = default) => math.float3(value, z);
         public static float2 ToFloat2(this Vector3 value) => ((float3)(value)).xy;
+        public static float4 ToFloat4(this Vector3 value) => new((float3)value, 0);
         public static void Deconstruct(this float2 value, out float x, out float y) => (x, y) = (value.x, value.y);
         public static void Deconstruct(this int2 value, out int x, out int y) => (x, y) = (value.x, value.y);
     }
