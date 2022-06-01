e0d127631f2b5f4ab4389b2087dbc678bd897b1d
Andrzej Więckowski <andywiecko@gmail.com>
2022-06-01 18:32:30 +0200

    refactor: remove deprecated files

--- Runtime/Core/Contracts/Contracts.Constraints.cs
+++ Runtime/Core/Contracts/Contracts.Constraints.cs
@@ -14,55 +14,6 @@ namespace andywiecko.PBD2D.Core
         Ref<NativeList<EdgeLengthConstraint>> Constraints { get; }
     }
 
-    public interface IEdgeBendingConstraint : IComponent
-    {
-        float Stiffness { get; }
-        Ref<NativeIndexedArray<Id<Point>, float2>> PredictedPositions { get; }
-        NativeIndexedArray<Id<Point>, float>.ReadOnly MassesInv { get; }
-        NativeIndexedArray<Id<Frame>, Frame>.ReadOnly Frames { get; }
-        NativeIndexedArray<Id<Frame>, float>.ReadOnly RestAngles { get; }
-    }
-
-    public interface IAttachmentConstraintRod : IComponent
-    {
-        bool LockOrientation { get; }
-        bool IsDirectedForward { get; }
-        Id<Edge> EdgeId { get; }
-        float2 AttachmentPosition { get; }
-        float2 AttachmentDirection { get; }
-        Ref<NativeIndexedArray<Id<Point>, float2>> PredictedPositions { get; }
-        NativeIndexedArray<Id<Edge>, Edge>.ReadOnly Edges { get; }
-    }
-
-    public interface IPointEdgeDistanceConstraint : IComponent
-    {
-        float Stiffness { get; }
-        float Weight { get; }
-        Id<Point> ConnecteePointId { get; }
-        Id<Edge> ConnecterEdgeId { get; }
-        Ref<NativeReference<float>> RestLength { get; }
-        Ref<NativeIndexedArray<Id<Point>, float2>> ConnecterPredictedPositions { get; }
-        Ref<NativeIndexedArray<Id<Point>, float2>> ConnecteePredictedPositions { get; }
-        NativeIndexedArray<Id<Point>, float>.ReadOnly ConnecterMassesInv { get; }
-        NativeIndexedArray<Id<Point>, float>.ReadOnly ConnecteeMassesInv { get; }
-        NativeIndexedArray<Id<Edge>, Edge>.ReadOnly ConnecterEdges { get; }
-    }
-
-    public interface IEdgeBendingConstraintRodConnector : IComponent
-    {
-        float Stiffness { get; }
-        float Weight { get; }
-        float RestAngle { get; }
-        Id<Edge> ConnecterEdgeId { get; }
-        Id<Edge> ConnecteeEdgeId { get; }
-        Ref<NativeIndexedArray<Id<Point>, float2>> ConnecterPredictedPositions { get; }
-        Ref<NativeIndexedArray<Id<Point>, float2>> ConnecteePredictedPositions { get; }
-        NativeIndexedArray<Id<Point>, float>.ReadOnly ConnecterMassesInv { get; }
-        NativeIndexedArray<Id<Point>, float>.ReadOnly ConnecteeMassesInv { get; }
-        NativeIndexedArray<Id<Edge>, Edge>.ReadOnly ConnecterEdges { get; }
-        NativeIndexedArray<Id<Edge>, Edge>.ReadOnly ConnecteeEdges { get; }
-    }
-
     public interface ITriangleAreaConstraints : IComponent
     {
         float Stiffness { get; }
@@ -86,17 +37,5 @@ namespace andywiecko.PBD2D.Core
         float2x2 AqqMatrix { get; }
         Ref<NativeReference<float2x2>> AMatrix { get; }
         Ref<NativeReference<Complex>> Rotation { get; }
-    }
-
-    public interface IContactConstraintRod : IComponent
-    {
-        float Stiffness { get; }
-        float ContactRadius { get; }
-        NativeIndexedArray<Id<Edge>, Edge>.ReadOnly Edges { get; }
-        NativeIndexedArray<Id<Point>, float>.ReadOnly MassesInv { get; }
-        Ref<NativeIndexedArray<Id<Point>, float2>> PredictedPositions { get; }
-        Ref<NativeIndexedArray<Id<Edge>, AABB>> AABBs { get; }
-        Ref<NativeIndexedList<Id<PotentialCollision>, IdPair<Edge>>> PotentialCollisions { get; }
-        Ref<NativeIndexedList<Id<ContactInfo>, ContactInfo>> ContactInfo { get; }
     }
 }
--- Runtime/Core/Structs/Contact.cs
+++
@@ -1,4 +0,0 @@
-namespace andywiecko.PBD2D.Core
-{
-    public readonly struct Contact { }
-}
