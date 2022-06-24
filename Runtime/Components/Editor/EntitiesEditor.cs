8f78b6bf2dba6f014c6c65dbe5080386de1eb211
Andrzej Więckowski <andywiecko@gmail.com>
2022-06-24 16:24:00 +0200

    feat: menu items for entities

---
+++ Runtime/Components/Editor/EntitiesEditor.cs
@@ -0,0 +1,31 @@
+using andywiecko.ECS;
+using UnityEditor;
+using UnityEngine;
+
+namespace andywiecko.PBD2D.Components.Editor
+{
+    public class EntitiesEditor : MonoBehaviour
+    {
+        // TODO: add source generator for these
+
+        private const string path = "GameObject/PBD2D/";
+
+        [MenuItem(path + nameof(TriMesh))]
+        public static void CreateTriMesh() => Create<TriMesh>();
+
+        [MenuItem(path + nameof(Ground))]
+        public static void CreateGround() => Create<Ground>();
+
+        private static void Create<T>() where T : Entity
+        {
+            Selection.activeObject = new GameObject(typeof(T).Name).AddComponent<T>();
+            EditorApplication.delayCall += subscribe;
+
+            static void subscribe()
+            {
+                EditorWindow.focusedWindow.SendEvent(new Event { keyCode = KeyCode.F2, type = EventType.KeyDown });
+                EditorApplication.delayCall -= subscribe;
+            }
+        }
+    }
+}
