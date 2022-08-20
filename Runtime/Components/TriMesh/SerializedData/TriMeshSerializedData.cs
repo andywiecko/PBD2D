2367518a8403fdebce884599a51ceaee2489f886
Andrzej Więckowski <andywiecko@gmail.com>
2022-08-20 15:02:44 +0200

    refactor: serialized data flatten structure

--- Runtime/Components/TriMesh/SerializedData/TriMeshSerializedData.cs
+++ Runtime/Components/TriMesh/SerializedData/TriMeshSerializedData.cs
@@ -1,9 +1,3 @@
-using andywiecko.BurstTriangulator;
-using andywiecko.PBD2D.Core;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -11,98 +5,9 @@ namespace andywiecko.PBD2D.Components
 {
     public abstract class TriMeshSerializedData : ScriptableObject
     {
-        [Serializable]
-        protected class Path : IEnumerable<float2>
-        {
-            [field: SerializeField]
-            public float2[] Data { get; private set; } = { };
-            public IEnumerator<float2> GetEnumerator() => (Data as IEnumerable<float2>).GetEnumerator();
-            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-            public Path(float2[] data) => Data = data;
-        }
-
-        public class ManagedInputData
-        {
-            public float2[] Positions;
-            public int[] Constraints;
-            public float2[] Holes;
-        }
-
         [field: SerializeField, HideInInspector] public Mesh Mesh { get; protected set; } = default;
         [field: SerializeField, HideInInspector] public float2[] Positions { get; protected set; } = { };
         [field: SerializeField, HideInInspector] public int[] Triangles { get; protected set; } = { };
         [field: SerializeField, HideInInspector] public Vector2[] UVs { get; protected set; } = { };
-
-        [field: SerializeField]
-        public Triangulator.TriangulationSettings Settings { get; private set; } = new();
-
-        public ManagedInputData InputData = new();
-
-        [SerializeField]
-        protected Path[] paths = { };
-
-        [SerializeField]
-        protected float2[] holes = { };
-
-#if UNITY_EDITOR
-        public void RegenerateMesh()
-        {
-            if (Mesh != null)
-            {
-                UnityEditor.AssetDatabase.RemoveObjectFromAsset(Mesh);
-            }
-            Mesh = CreateMesh();
-            Mesh.name = "Generated Mesh";
-            UnityEditor.AssetDatabase.AddObjectToAsset(Mesh, this);
-
-            Mesh CreateMesh()
-            {
-                var mesh = new Mesh();
-                mesh.SetVertices(Positions.Select(i => (Vector3)i.ToFloat3()).ToList());
-                mesh.SetTriangles(Triangles, submesh: 0);
-                mesh.SetUVs(0, UVs);
-                return mesh;
-            }
-        }
-#endif
-
-        private void GetPointsFromPaths()
-        {
-            InputData.Positions = paths.SelectMany(i => i.Data).ToArray();
-
-            var cnstr = new List<int>();
-            var offset = 0;
-            foreach (var p in paths)
-            {
-                var n = p.Data.Length;
-                cnstr.AddRange(Enumerable.Range(0, n).SelectMany(i => new[] { i + offset, (i + 1) % n + offset }).ToArray());
-                offset += n;
-            }
-
-            InputData.Constraints = cnstr.ToArray();
-
-            InputData.Holes = holes;
-
-#if UNITY_EDITOR
-            UnityEditor.EditorApplication.delayCall -= GetPointsFromPaths;
-#endif 
-        }
-
-        protected virtual void OnValidate()
-        {
-#if UNITY_EDITOR
-            UnityEditor.EditorApplication.delayCall += GetPointsFromPaths;
-#endif
-        }
-
-        public void CopyDataFromTriangulation(Triangulator triangulator)
-        {
-            Triangles = triangulator.Output.Triangles.ToArray();
-            Positions = triangulator.Output.Positions.ToArray();
-
-            UpdateUVs();
-        }
-
-        protected abstract void UpdateUVs();
     }
 }
