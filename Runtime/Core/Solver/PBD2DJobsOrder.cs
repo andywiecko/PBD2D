0551118904076d70e8cf34862b2e907948db4c7d
Andrzej Więckowski <andywiecko@gmail.com>
2022-08-25 13:08:44 +0200

    refactor: simplify pbd2d jobs order

--- Runtime/Core/Solver/PBD2DJobsOrder.cs
+++ Runtime/Core/Solver/PBD2DJobsOrder.cs
@@ -1,14 +1,10 @@
 using andywiecko.ECS;
+#if UNITY_EDITOR
 using andywiecko.ECS.Editor;
-using Newtonsoft.Json.Linq;
+#endif
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using Unity.Jobs;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 using UnityEngine;
 
 namespace andywiecko.PBD2D.Core
@@ -43,41 +39,10 @@ namespace andywiecko.PBD2D.Core
             }
         }
 
-        private List<Type> GetSerializedTypes() => new[] { frameStart, substep, frameEnd }
-            .SelectMany(i => i)
-            .Select(i => i.Type)
-            .ToList();
-
-        private List<SerializedType> GetListAtStep(SimulationStep step) => step switch
-        {
-            SimulationStep.FrameStart => frameStart,
-            SimulationStep.Substep => substep,
-            SimulationStep.FrameEnd => frameEnd,
-            _ => default,
-        };
-
-        private void SetListAtStep(SimulationStep step, List<SerializedType> list)
-        {
-            switch (step)
-            {
-                case SimulationStep.FrameStart:
-                    frameStart = list;
-                    break;
-
-                case SimulationStep.Substep:
-                    substep = list;
-                    break;
-
-                case SimulationStep.FrameEnd:
-                    frameEnd = list;
-                    break;
-            }
-        }
-
         [field: SerializeField, HideInInspector]
         public string[] TargetAssemblies { get; private set; } = { };
-
 #if UNITY_EDITOR
+        private IEnumerable<Type> TargetTypes => TypeCacheUtils.Systems.GetTypes(TargetAssemblies);
         [SerializeField]
         private UnityEditorInternal.AssemblyDefinitionAsset[] targetAssemblies = { };
 #endif
@@ -91,112 +56,117 @@ namespace andywiecko.PBD2D.Core
         [SerializeField]
         private List<UnconfiguredType> undefinedTypes = new();
 
-        private readonly Dictionary<SimulationStep, List<Type>> jobsOrder = new();
-
-        public void RegenerateJobsOrder()
-        {
-            jobsOrder.Clear();
-
-            foreach (var s in SystemExtensions.GetValues<SimulationStep>())
-            {
-                var list = GetListAtStep(s);
-                if (list is null) continue;
-                var types = new List<Type>(capacity: list.Count);
-                foreach (var st in list)
-                {
-                    types.Add(st.Type);
-                }
-                jobsOrder.Add(s, types);
-            }
-        }
-
-        private void Awake() => ValidateTypes();
+#if UNITY_EDITOR
+        private void Awake() => ValidateSerializedTypes();
 
         private void OnValidate()
         {
-            TargetAssemblies = targetAssemblies?
-                .Where(i => i != null)
-                .Select(i => JObject.Parse(i.text)["name"].ToString()).ToArray();
+            TargetAssemblies = targetAssemblies?.GetNames().ToArray();
 
-            ValidateTypes();
+            // HACK:
+            //   For unknown reason static dicts don't survive when saving asset,
+            //   but OnValidate is called during save.
+            if (TypeCacheUtils.Systems.GuidToType.Count != 0)
+            {
+                ValidateSerializedTypes();
+            }
         }
 
-        private void ValidateTypes()
+        private void ValidateSerializedTypes()
         {
-            // HACK:
-            //   For unknown reason static dicts don't survive when saving assest,
-            //   but OnValidate is called during save.
-            if (TypeCacheUtils.Systems.GuidToType.Count == 0)
-            {
-                return;
-            }
+            RemoveBadTypes();
+            AssignEnabledTypes();
+            FillUndefinedTypes();
+            ValidateEnabledTypes();
+        }
 
-            var targetTypes = TargetAssemblies
-                .Select(i => Assembly.Load(i))
-                .SelectMany(i => TypeCacheUtils.Systems.AssemblyToTypes[i]);
-            var targetGuids = targetTypes.Select(i => TypeCacheUtils.Systems.TypeToGuid[i]);
+        private void RemoveBadTypes()
+        {
+            frameStart.RemoveAll(i => !TypeCacheUtils.Systems.GuidToType.ContainsKey(i.Guid));
+            substep.RemoveAll(i => !TypeCacheUtils.Systems.GuidToType.ContainsKey(i.Guid));
+            frameEnd.RemoveAll(i => !TypeCacheUtils.Systems.GuidToType.ContainsKey(i.Guid));
+            undefinedTypes.RemoveAll(i => !TypeCacheUtils.Systems.GuidToType.ContainsKey(i.Type.Guid));
 
+            frameStart = frameStart.DistinctBy(i => i.Guid).ToList();
+            substep = substep.DistinctBy(i => i.Guid).ToList();
+            frameEnd = frameEnd.DistinctBy(i => i.Guid).ToList();
+        }
+
+        private void AssignEnabledTypes()
+        {
             var typesToAssign = undefinedTypes.Where(t => t.Step != SimulationStep.Undefined);
             foreach (var u in typesToAssign)
             {
-                GetListAtStep(u.Step).Add(u.Type);
-            }
-
-            foreach (var step in SystemExtensions.GetValues<SimulationStep>().Except(new[] { SimulationStep.Undefined }))
-            {
-                var list = GetListAtStep(step)
-                    .DistinctBy(i => i.Guid)
-                    .Where(i => i.Type is not null)
-                    .ToList();
-
-                list.RemoveAll(i => !targetGuids.Contains(i.Guid));
-
-                SetListAtStep(step, list);
-            }
-
-            undefinedTypes.Clear();
-            foreach (var t in targetTypes.Except(GetSerializedTypes()))
-            {
-                undefinedTypes.Add(new(t, TypeCacheUtils.Systems.TypeToGuid[t]));
-            }
-
-            foreach (var step in SystemExtensions.GetValues<SimulationStep>())
-            {
-                var list = GetListAtStep(step);
-                if (list is not null)
+                var list = u.Step switch
                 {
-                    foreach (var l in list)
-                    {
-                        l.Validate(TypeCacheUtils.Systems.GuidToType[l.Guid]);
-                    }
+                    SimulationStep.FrameStart => frameStart,
+                    SimulationStep.Substep => substep,
+                    SimulationStep.FrameEnd => frameEnd,
+                    _ => throw new Exception()
+                };
+
+                list.Add(u.Type);
+            }
+        }
+
+        private void FillUndefinedTypes()
+        {
+            undefinedTypes.Clear();
+
+            var enabledTypes = frameStart.Concat(substep).Concat(frameEnd);
+
+            foreach (var t in TargetTypes)
+            {
+                if (!enabledTypes.Select(i => i.Type).Contains(t) &&
+                    TypeCacheUtils.Systems.TypeToGuid.TryGetValue(t, out var guid))
+                {
+                    undefinedTypes.Add(new(t, guid));
                 }
             }
         }
 
+        private void ValidateEnabledTypes()
+        {
+            var enabledTypes = frameStart.Concat(substep).Concat(frameEnd);
+            foreach (var t in enabledTypes)
+            {
+                t.Validate(TypeCacheUtils.Systems.GuidToType[t.Guid]);
+            }
+        }
+#endif
+
         public override void GenerateJobs(ISolver solver, IWorld world)
         {
-            RegenerateJobsOrder();
-
             var jobs = solver.Jobs;
-            jobs.AddRange(GetJobsFor(SimulationStep.FrameStart, world));
-            for (int step = 0; step < world.ConfigurationsRegistry.Get<PBDConfiguration>().StepsCount; step++)
-            {
-                jobs.AddRange(GetJobsFor(SimulationStep.Substep, world));
-            }
-            jobs.AddRange(GetJobsFor(SimulationStep.FrameEnd, world));
-        }
+            var systems = world.SystemsRegistry;
 
-        private List<Func<JobHandle, JobHandle>> GetJobsFor(SimulationStep step, IWorld world)
-        {
-            var jobs = new List<Func<JobHandle, JobHandle>>();
-            foreach (var type in jobsOrder[step])
+            foreach (var type in frameStart.Select(i => i.Type))
             {
-                if (world.SystemsRegistry.TryGetSystem(type, out var system))
+                if (systems.TryGetSystem(type, out var system))
                 {
                     jobs.Add(system.Schedule);
                 }
             }
-            return jobs;
+
+            var stepsCount = world.ConfigurationsRegistry.Get<PBDConfiguration>().StepsCount;
+            for (int step = 0; step < stepsCount; step++)
+            {
+                foreach (var type in substep.Select(i => i.Type))
+                {
+                    if (systems.TryGetSystem(type, out var system))
+                    {
+                        jobs.Add(system.Schedule);
+                    }
+                }
+            }
+
+            foreach (var type in frameEnd.Select(i => i.Type))
+            {
+                if (systems.TryGetSystem(type, out var system))
+                {
+                    jobs.Add(system.Schedule);
+                }
+            }
         }
     }
 }
