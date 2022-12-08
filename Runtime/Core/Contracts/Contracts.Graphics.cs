34eb3739da56bb7cc24d31ea9c0911ea739dc8ee
andywiecko <andywiecko@gmail.com>
2022-12-08 17:13:18 +0100

    feat: edge mesh renderer
    Basic renderer for `EdgeMesh` (without interpolation). MR provides default renderer prefab and material as well.

--- Runtime/Core/Contracts/Contracts.Graphics.cs
+++ Runtime/Core/Contracts/Contracts.Graphics.cs
@@ -1,5 +1,6 @@
 using andywiecko.BurstCollections;
 using andywiecko.ECS;
+using System.Collections.Generic;
 using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine;
@@ -12,5 +13,13 @@ namespace andywiecko.PBD2D.Core
         Mesh Mesh { get; }
         Ref<NativeIndexedArray<Id<Point>, float2>> Positions { get; }
         Ref<NativeArray<float3>> MeshVertices { get; }
+    }
+
+    public interface IEdgeMeshRenderer : IComponent
+    {
+        IReadOnlyList<LineRenderer> Renderers { get; }
+        Ref<NativeStackedLists<Id<Point>>> Segments { get; }
+        Ref<NativeIndexedArray<Id<Point>, float2>> Positions { get; }
+        Ref<NativeStackedLists<float3>> MeshVertices { get; }
     }
 }
