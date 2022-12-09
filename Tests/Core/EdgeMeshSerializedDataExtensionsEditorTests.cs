6694ca0c516344006280b5273918b12d98a61d4d
andywiecko <andywiecko@gmail.com>
2022-12-09 16:17:25 +0100

    feat: edge mesh to stencils extension

--- Tests/Core/EdgeMeshSerializedDataExtensionsEditorTests.cs
+++ Tests/Core/EdgeMeshSerializedDataExtensionsEditorTests.cs
@@ -78,6 +78,79 @@ namespace andywiecko.PBD2D.Editor.Tests
                 .CreateInstance<FakeSerializedData>()
                 .SetPositionsCount(count)
                 .SetEdges(edges)
-                .ToSegments(Allocator.Temp);
+                .ToSegments(Allocator.Temp)
+            ;
+
+        private static Stencil stl(int i, int j, int k) => new(new(i), new(j), new(k));
+        private static readonly TestCaseData[] extensionToStencilsTestCaseData =
+        {
+            new(2, new int[]{ 0, 1 })
+            {
+                TestName = "Test case 1 (two points)",
+                ExpectedResult = new Stencil[] { }
+            },
+            new(4, new int[]{ 0, 1, 1, 2, 2, 3 })
+            {
+                TestName = "Test case 2 (line)",
+                ExpectedResult = new Stencil[] { stl(0, 1, 2), stl(1, 2, 3) }
+            },
+            new(4, new int[]{ 0, 1, 1, 2, 2, 3, 3, 0 })
+            {
+                TestName = "Test case 3 (loop)",
+                ExpectedResult = new Stencil[] { stl(1, 0, 3), stl(0, 1, 2), stl(1, 2, 3), stl(2, 3, 0) }
+            },
+            new(5, new int[]{ 0, 1, 0, 2, 0, 3, 0, 4 })
+            {
+                TestName = "Test case 4 (cross)",
+                ExpectedResult = new Stencil[]
+                {
+                    stl(3, 0, 4),
+                    stl(2, 0, 4),
+                    stl(1, 0, 4),
+                    stl(2, 0, 3),
+                    stl(1, 0, 3),
+                    stl(1, 0, 2),
+                }
+            },
+            new(10, new int[]
+            {
+                0, 1,
+                1, 2,
+                2, 3,
+                2, 4,
+                4, 5,
+                1, 6,
+                6, 7,
+                6, 8,
+                8, 9,
+            })
+            {
+                TestName = "Test case 5 (tree)",
+                ExpectedResult = new Stencil[]
+                {
+                    stl(2, 1, 6),
+                    stl(0, 1, 6),
+                    stl(0, 1, 2),
+                    stl(3, 2, 4),
+                    stl(1, 2, 4),
+                    stl(1, 2, 3),
+                    stl(2, 4, 5),
+                    stl(7, 6, 8),
+                    stl(1, 6, 8),
+                    stl(1, 6, 7),
+                    stl(6, 8, 9),
+                }
+            }
+        };
+
+        [Test, TestCaseSource(nameof(extensionToStencilsTestCaseData))]
+        public IEnumerable<Stencil> ExtensionToStencilsTest(int count, int[] edges) =>
+            ScriptableObject
+                .CreateInstance<FakeSerializedData>()
+                .SetPositionsCount(count)
+                .SetEdges(edges)
+                .ToStencils(Allocator.Temp)
+                .AsArray()
+            ;
     }
 }
